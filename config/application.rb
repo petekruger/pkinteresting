0b33a1cf1f52cd19a5b875afbb5f93f0cd8aa948
Peter Kruger <peter@hivesocialgiving.com>
2015-02-06 17:25:21 -0700

    implemented devise for users

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Pkinteresting
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # required for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
